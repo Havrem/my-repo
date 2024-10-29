1b8a8e837393d9358fccb26b7f80e92d45717aa5
Havrem <144966173+Havrem@users.noreply.github.com>
2024-10-29 12:57:02 +0100

    first commit

---
+++ src/ClassOne.java
@@ -0,0 +1,15 @@
+public class ClassOne {
+    private String stuff;
+
+    public ClassOne(String stuff) {
+        this.stuff = stuff;
+    }
+
+    public String getStuff() {
+        return stuff;
+    }
+
+    public void setStuff(String stuff) {
+        this.stuff = stuff;
+    }
+}
---
+++ src/ClassTwo.java
@@ -0,0 +1,15 @@
+public class ClassTwo {
+    private String stuff;
+
+    public ClassTwo(String stuff) {
+        this.stuff = stuff;
+    }
+
+    public String getStuff() {
+        return stuff;
+    }
+
+    public void setStuff(String stuff) {
+        this.stuff = stuff;
+    }
+}
